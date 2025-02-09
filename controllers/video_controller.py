074fc805cf6d77ee7a9f6b8398a692b538a2614c
Rodrigo Zanel <rodrigozanelsp@gmail.com>
2025-02-08 16:04:46 -0800

    feat(gun-detector): Adicionada função para comparar frames e evitar multiplas detecçoes de frames sequenciais

--- controllers/video_controller.py
+++ controllers/video_controller.py
@@ -1,8 +1,13 @@
 # controllers/video_controller.py
 import time
 import cv2
+#from skimage.metrics import structural_similarity as ssim
+# import structural_similarity from scikit-image
+import numpy as np
 
 class VideoController:
+    similarity_threshold = 0.50
+    last_processed_frame = None
     """
     Handles the video stream: reading frames, passing them to the detector,
     and saving images when a detection is made (with a configurable save interval).
@@ -33,24 +38,51 @@ class VideoController:
         processed_frame, detections = self.detector.analyze_frame(frame)
 
         # If detections exist and we haven't saved recently, save the frame.
-        if detections:
+        if detections:            
             current_time = time.time()
             if current_time - self.last_save_time >= self.save_interval:
                 # You might choose which detection to use. Here, we use the first.
                 label = detections[0]['label']
+
+                # Check if the current frame is similar to the last one.
+                _is_similar = self.last_processed_frame is not None and self.is_similar(self.last_processed_frame, processed_frame, self.similarity_threshold)
+                if _is_similar:
+                    return processed_frame, detections
+
+                self.last_processed_frame = processed_frame
+
                 self.image_saver.save_image(frame, label)
                 self.last_save_time = current_time
+        else:
+            self.last_processed_frame = None
 
         return processed_frame, detections
+            
+    
+    def mse(self, imageA, imageB):
+        # Compute the Mean Squared Error between two images
+        err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+        err /= float(imageA.shape[0] * imageA.shape[1])
+        return err
+
+    def is_similar(self, frame1, frame2, threshold):
+        """Compara dois frames usando SSIM para detectar mudanças."""
+        gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+        gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+        #score, _ = ssim(gray1, gray2, full=True)
+        score = self.mse(gray1, gray2)
+        return score > threshold
 
     def generate_frames(self):
         """
         Generator that yields JPEG-encoded frames.
         """
+        _is_similar = False
         while True:
             processed_frame, _ = self.process_frame()
             if processed_frame is None:
                 break
+
             ret, buffer = cv2.imencode('.jpg', processed_frame)
             frame_bytes = buffer.tobytes()
             yield (b'--frame\r\n'
